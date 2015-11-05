6d72406a6b27e6938bb41968e8acfeaf4db34b24
Loint <ntlzz5793@hotmail.com>
2015-11-05 21:10:31 +0700

    server sent back result to client

--- client2.c
+++ client2.c
@@ -5,67 +5,71 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#define BUFFSIZE 32
-/// Client chuyen mot chuoi ki tu, sau khi an enter n gui cho server roi ket thuc
+#define BUFFSIZE 2048
+
 void Die(char *mess) 
 {
-	perror(mess); 
- 	exit(1); 
+    perror(mess); 
+    exit(1); 
 }
 int main(int argc, char *argv[]) 
 {
-while(1){
-	int sock;
-	struct sockaddr_in echoserver;
-	char buffer[BUFFSIZE];
-	unsigned int echolen;
-	int received = 0;
-	if (argc != 3) 
-	{
-		fprintf(stderr, "USAGE: TCPecho <server_ip> <word> <port>\n");
-		exit(1);
-	}
-/* Create the TCP socket */
-	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) 
-	{
-		Die("Failed to create socket");
-	}
-/* Construct the server sockaddr_in structure */
-	memset(&echoserver, 0, sizeof(echoserver));
-	echoserver.sin_family = AF_INET;
-	echoserver.sin_addr.s_addr = inet_addr(argv[1]);
-	echoserver.sin_port = htons(atoi(argv[2]));
-/* Establish connection */
-	if (connect(sock,(struct sockaddr *) &echoserver,sizeof(echoserver)) < 0) 
-	{
-		Die("Failed to connect with server");
-	}
-/* Send the word to the server */
-	//tao mot chuoi s, chuoi s nhan ki tu tu ban phim, roi chuyen cho phia server
-	char s[100];
-	fgets(s,100,stdin);
-	echolen = strlen(s);
-	if (send(sock, s, echolen, 0) != echolen) 
-	{
-		Die("Mismatch in number of sent bytes");
-	}
-/* Receive the word back from the server */
-	fprintf(stdout, "Message from server: ");
-	while (received < echolen) 
-	{
-		int bytes = 0;
-		if ((bytes = recv(sock, buffer, BUFFSIZE-1, 0)) < 1) 
-		{
-			Die("Failed to receive bytes from server");
-		}
-		received += bytes;
-		buffer[bytes] = '\0';
-		/* Assure null terminated string */
-		fprintf(stdout, buffer);
-	}
-	fprintf(stdout, "\n");
-	close(sock);
-	if(strcmp(s,"exit") == 0)
-	exit(0);
-}
+    
+    int sock;
+    struct sockaddr_in echoserver;
+    char buffer[BUFFSIZE];
+    unsigned int echolen;
+    int received = 0;
+    if (argc != 3) 
+    {
+        fprintf(stderr, "USAGE: TCPecho <server_ip> <word> <port>\n");
+        exit(1);
+    }
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) 
+    {
+        Die("Failed to create socket");
+    }
+    memset(&echoserver, 0, sizeof(echoserver));
+    echoserver.sin_family = AF_INET;
+    echoserver.sin_addr.s_addr = inet_addr(argv[1]);
+    echoserver.sin_port = htons(atoi(argv[2]));
+    if (connect(sock,(struct sockaddr *) &echoserver,sizeof(echoserver)) < 0) 
+    {
+        Die("Failed to connect with server");
+    }
+    char s[100];
+    fgets(s,100,stdin);
+    echolen = strlen(s);
+
+    /*  send() from client; */
+    if (send(sock, s, echolen, 0) != echolen) 
+    {
+        Die("Mismatch in number of sent bytes");
+    }
+    fprintf(stdout, "Message from server: ");
+    while (received < echolen) 
+    {
+        int bytes = 0;
+        /* recv() from server; */
+        if ((bytes = recv(sock, buffer, BUFFSIZE-1, 0)) < 1) 
+        {
+            Die("Failed to receive bytes from server");
+        }
+        received += bytes;
+        buffer[bytes] = '\0';
+        /* Assure null terminated string */
+        fprintf(stdout, buffer);
+    }
+
+    int bytes = 0;
+    do {
+        buffer[bytes] = '\0';
+        printf("%s\n", buffer);
+    } while(bytes = recv(sock, buffer, BUFFSIZE-1, 0));
+    printf("%s\n", buffer);
+    printf("\n");
+    //close(sock);
+    if(strcmp(s,"exit") == 0)
+    exit(0);
+
 }
